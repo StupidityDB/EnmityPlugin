4d09f576a40fc2e39fed722f1fef19d6ba78d635
Acquite <acquitefx@gmail.com>
2023-03-13 23:30:46 +0000

    Fix using hash instead of build in updater

--- src/common/update.ts
+++ src/common/update.ts
@@ -1,4 +1,4 @@
-import { name, links, version, build } from '../../manifest.json';
+import { name, links, version, hash } from '../../manifest.json';
 import { Dialog } from 'enmity/metro/common';
 import { reload } from 'enmity/api/native';
 import tryCallback from './try_callback';
@@ -14,15 +14,15 @@ async function checkForUpdates(): Promise<void> {
         const potentialExternalHash = content.match(/hash:"(.*?)"/)
 
         if (!potentialExternalVersion && !potentialExternalHash)
-            return failureUpdate(name, [version, build]);
+            return failureUpdate(name, [version, hash]);
 
         const externalVersion = potentialExternalVersion && potentialExternalVersion[0];
         const externalHash = potentialExternalHash && potentialExternalHash[1]
 
         if (externalVersion && (externalVersion != version)) return showUpdateDialog(url, externalVersion, 'version');
-        if (externalHash && (externalHash != build)) return showUpdateDialog(url, externalHash, 'build');
-        return noUpdates(name, [version, build]);
-    }, [links, version, build], name, 'checking if latest version at', 'the async check for updates callback');
+        if (externalHash && (externalHash != hash)) return showUpdateDialog(url, externalHash, 'build');
+        return noUpdates(name, [version, hash]);
+    }, [links, version, hash], name, 'checking if latest version at', 'the async check for updates callback');
 }
 
 const showUpdateDialog = (url: string, updateLabel: string, updateType: string): void => {
